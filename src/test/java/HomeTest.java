7eeba5336f681b8a382b105ef3215f2eb57077ce
LaraLark <86426630+LaraLark@users.noreply.github.com>
2021-07-03 22:04:48 +0300

    created HomeTest

---
+++ src/test/java/HomeTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class HomeTest {
+    WebDriver wd;
+    @BeforeMethod
+    public void precondition(){
+      wd = new ChromeDriver() ;
+      wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+    }
+
+    @Test
+    public void findElement(){
+        List<WebElement> list = wd.findElements(By.tagName("a"));
+        int i = list.size()-2;
+        WebElement element = list.get(i);
+        element.click();
+
+    }
+
+    @AfterMethod
+    public void postCondition(){
+      wd.findElement(By.tagName("a"));
+      wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+
+
+    }
+}
